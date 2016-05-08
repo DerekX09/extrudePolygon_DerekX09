67ac0ae8329447b7e0e772840b66df1c5779e429
madhephaestus <mad.hephaestus@gmail.com>
2016-05-08 18:02:21 -0400

    better demo

--- extrudePolygon.groovy
+++ extrudePolygon.groovy
@@ -1,9 +1,9 @@
-double size=40;
+
 //create an extruded polygon
-CSG polygon = Extrude.points(new Vector3d(0, 0, size),// This is the  extrusion depth
+CSG polygon = Extrude.points(new Vector3d(0, 0, 40),// This is the  extrusion depth
                 new Vector3d(0,0),// All values after this are the points in the polygon
-                new Vector3d(size*2,0),// Bottom right corner
-                new Vector3d(size*1.5,size),// upper right corner
-                new Vector3d(size/2,size)// upper left corner
+                new Vector3d(80,0),// Bottom right corner
+                new Vector3d(60,40),// upper right corner
+                new Vector3d(20,40)// upper left corner
         );
 return polygon
